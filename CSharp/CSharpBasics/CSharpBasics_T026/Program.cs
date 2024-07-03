c5439a84e82cfe16202e3843d3df53f49c06fae3
shivangKheradiya <shivangatul@gmail.com>
2024-07-04 02:27:53 +0530

    CSharpBasics_T026 To CSharpBasics_T028

--- CSharp/CSharpBasics/CSharpBasics_T026/Program.cs
+++ CSharp/CSharpBasics/CSharpBasics_T026/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpBasics_T026
 {
@@ -10,6 +6,89 @@ namespace CSharpBasics_T026
     {
         static void Main(string[] args)
         {
+            Employees employees = new Employees();
+            try
+            {
+                employees.SetEmployeeID(-5);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                employees.SetEmployeeID(55);
+                employees.SetFName(null);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                employees.SetFName("Shiva");
+                employees.SetLName("");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            employees.SetLName("Jagdish");
+
+            Console.WriteLine(employees.GetFName());
+            Console.WriteLine(employees.GetLName());
+            Console.WriteLine(employees.GetEmployeeID());
+        }
+    }
+
+    public class Employees
+    {
+        // Encapsulate the the class means control the behavior of the fields based on the requirement.
+        private int employeeID;
+        private string fName;
+        private string lName;
+
+
+        public void SetEmployeeID(int id)
+        {
+            if (id < 0)
+                throw new Exception("Exception: ID Can't Be Negative");
+            else
+                employeeID = id;
+        }
+
+        public int GetEmployeeID()
+        {
+            return employeeID;
+        }
+
+        public void SetFName(string fstName)
+        {
+            if (!string.IsNullOrEmpty(fstName))
+                fName = fstName;
+            else
+                throw new Exception("Exception: First Name Can't be null or empty.");
+        }
+
+        public string GetFName()
+        {
+            return string.IsNullOrEmpty(fName) ? "No Fname." : fName;
+        }
+
+        public void SetLName(string lstName)
+        {
+            if (!string.IsNullOrEmpty(lstName))
+                lName = lstName;
+            else
+                throw new Exception("Exception: Last Name Can't be null or empty.");
+        }
+
+        public string GetLName()
+        {
+            return string.IsNullOrEmpty(lName) ? "No Lname." : lName;
         }
     }
 }
--- CSharp/CSharpBasics/CSharpBasics_T027/Program.cs
+++ CSharp/CSharpBasics/CSharpBasics_T027/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSharpBasics_T027
 {
@@ -10,6 +6,100 @@ namespace CSharpBasics_T027
     {
         static void Main(string[] args)
         {
+            Employees employees = new Employees();
+            try
+            {
+                employees.EmployeeID = -5;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                employees.EmployeeID = 55;
+                employees.FName = null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                employees.FName = "Shiva";
+                employees.LName = "";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            employees.LName = "Jagdish";
+
+            Console.WriteLine(employees.FName);
+            Console.WriteLine(employees.LName);
+            Console.WriteLine(employees.EmployeeID);
+            Console.WriteLine(employees.FullName);
+
+            // employees.FullName Can't be set from following code
+            // employees.FullName = null;
+        }
+
+        public class Employees
+        {
+            // Encapsulate the the class means control the behavior of the fields based on the requirement.
+            private int employeeID;
+            private string fName;
+            private string lName;
+
+            // Automatic implimented property
+            public string EmailId { get; set; }
+
+            public int EmployeeID
+            {
+                get { return employeeID; }
+                set
+                {
+                    if (value < 0)
+                        throw new Exception("Exception: ID Can't Be Negative");
+                    else
+                        employeeID = value;
+                }
+            }
+
+            public string FName
+            {
+                get { return string.IsNullOrEmpty(fName) ? "No Fname." : fName; }
+                set
+                {
+                    if (!string.IsNullOrEmpty(value))
+                        fName = value;
+                    else
+                        throw new Exception("Exception: First Name Can't be null or empty.");
+                }
+            }
+
+            public string LName
+            {
+                get { return string.IsNullOrEmpty(lName) ? "No Lname." : lName; }
+                set
+                {
+                    if (!string.IsNullOrEmpty(value))
+                        lName = value;
+                    else
+                        throw new Exception("Exception: Last Name Can't be null or empty.");
+                }
+            }
+
+            public string FullName
+            {
+                get
+                {
+                    return FName + " " + LName;
+                }
+            }
         }
     }
 }
