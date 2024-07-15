6426380cf90c25e90e9336b17c91442c1e24933b
shivangKheradiya <shivangatul@gmail.com>
2024-07-16 01:15:51 +0530

    CSharpBasics_T029 To CSharpBasics_T030

--- CSharp/CSharpBasics/CSharpBasics_T029/Program.cs
+++ CSharp/CSharpBasics/CSharpBasics_T029/Program.cs
@@ -10,6 +10,56 @@ namespace CSharpBasics_T029
     {
         static void Main(string[] args)
         {
+            Student student = new Student(); // Will be destroyed once the scope is over
+            StudentCls studentCls = new StudentCls(); // Will remain in the memory untill GC Run
+            student.Id = 5;
+            studentCls.Id = 6;
+            student.Name = "S";
+            studentCls.Name = "S CLS";
+
+            int i = 10; // Will be destroyed once the scope is over
+
+            if (i == 10) 
+            {
+                int j = 1; // Will be destroyed once the scope is over
+                Student newStudent = student; // Will be destroyed once the scope is over
+                newStudent.Id = 7;
+                StudentCls newStudentCls = studentCls; // Will remain in the memory untill GC Run
+                newStudentCls.Id = 8;
+
+                Console.WriteLine(student.Id); 
+                Console.WriteLine(studentCls.Id); // Original value is modified as the class is reference type
+
+                studentCls.Id = 9;
+                Console.WriteLine(newStudentCls.Id); // value in copied variable is modified as the class is reference type
+            } // j,newStudent will be destroyed
+
+        } // i, student will be destroyed
+    }
+
+    public struct Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // ~Student() { } is not possible
+
+        public Student(int id,string name) // It must have parameters, Parameter less is not possible
+        {
+            Id = id;
+            Name = name;
         }
     }
+
+    public sealed class StudentCls // Sealed class can't Inherrite
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public StudentCls() { } // It's not possible in case of structures, so it must have parameters in the structures
+
+        ~StudentCls() { } // It's not possible in case of structures
+    }
+
+    // public class ExStudent : StudentCls { } Sealed class can't Inherrite
+    // public struct ExStudent : Student { } Inherritance is not possible for struct
 }
--- CSharp/CSharpBasics/CSharpBasics_T030/Program.cs
+++ CSharp/CSharpBasics/CSharpBasics_T030/Program.cs
@@ -10,6 +10,60 @@ namespace CSharpBasics_T030
     {
         static void Main(string[] args)
         {
+            IStudent student = new Student(); // It works similar to polymorphysm
+            student.Name = "Test";
+            student.Id = 1;
+            // student.LastName = "Test"; is not possible because student variable is of type IStudent
+            student.print();
+
+            // IStudent student = new IStudent(); is not possible
+        }
+    }
+
+    public interface IStudent // Contains only decleration
+    {
+        string Name { get; set; } //It's public by default
+        int Id { get; set; }
+
+        // int id; Fields are not possible for interfaces
+        void print();
+    }
+
+    public interface HasLastName
+    {
+        string LastName { get; set; }
+    }
+
+    public interface IExtStudent : HasLastName, IStudent // Interfaces can inherrite from more than one interface
+    {
+
+    }
+
+    public class ExtStudent : IExtStudent // Interfaces can have all inherrite methods from all the interface
+    {
+        public string LastName { get ; set ; }
+        public string Name { get ; set ; }
+        public int Id { get ; set ; }
+
+        public void print()
+        {
+            Console.WriteLine(Name + " has ID " + Id);
+        }
+    }
+
+    // public interface IExtStudent : Student { } interface can't inheritate from class
+
+    // Iterface implimentation must match all the methods signature from interface
+    // Class Allows multiple interface inheritance 
+    public class Student : IStudent , HasLastName
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string LastName { get; set ; }
+
+        public void print()
+        {
+            Console.WriteLine(Name + " has ID " + Id);
         }
     }
 }
