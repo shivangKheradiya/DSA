b1af0753e037b09e8344ed06a234b7ef180f1632
Shivang Kheradiya <31017420+shivangKheradiya@users.noreply.github.com>
2024-12-28 19:13:18 +0000

    Git Codespace is used

---
+++ CSharp/CSharpBasicsX/CSharpBasics_T040/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace CSharpBasics_T040
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
