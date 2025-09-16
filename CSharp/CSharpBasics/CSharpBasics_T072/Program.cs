7d8fc39364163bf992aaf120b5aed6fcce554796
Shivang Kheradiya <31017420+shivangKheradiya@users.noreply.github.com>
2025-09-16 10:13:35 +0000

    CSharpBasics_T072 to CSharpBasics_T073

--- CSharp/CSharpBasics/CSharpBasics_T072/Program.cs
+++ CSharp/CSharpBasics/CSharpBasics_T072/Program.cs
@@ -10,6 +10,33 @@ namespace CSharpBasics_T072
     {
         static void Main(string[] args)
         {
+            Dictionary<int, Customer> dictCust = new Dictionary<int, Customer>();
+            Customer a = new Customer(){ ID = 1, Name = "A", Salary = 5000 };
+            Customer b = new Customer(){ ID = 2, Name = "B", Salary = 3000 };
+            Customer c = new Customer(){ ID = 3, Name = "C", Salary = 4000 };
+            dictCust.Add(a.ID,a);
+            dictCust.Add(b.ID,b);
+            dictCust.Add(c.ID,c);
+
+            foreach (KeyValuePair<int, Customer> item in dictCust)
+            {
+                Console.WriteLine("ID : "+ item.Key + " , Name : " + item.Value.Name);               
+            }
+
+            foreach (Customer cust in dictCust.Values)
+            {
+                Console.WriteLine("ID : "+ cust.ID + " , Name : " + cust.Name);               
+            }
+
+            Console.WriteLine("Member with ID 1 :" + dictCust[1].Name);
+            Console.WriteLine("Member with Name B :" + dictCust.FirstOrDefault(x=>x.Name == "B")?.ID );
         }
+    }
+
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
     }
 }
--- CSharp/CSharpBasics/CSharpBasics_T073/Program.cs
+++ CSharp/CSharpBasics/CSharpBasics_T073/Program.cs
@@ -10,6 +10,56 @@ namespace CSharpBasics_T073
     {
         static void Main(string[] args)
         {
+            Dictionary<int, Customer> dictCust = new Dictionary<int, Customer>();
+            Customer a = new Customer(){ ID = 1, Name = "A", Salary = 5000 };
+            Customer b = new Customer(){ ID = 2, Name = "B", Salary = 3000 };
+            Customer c = new Customer(){ ID = 3, Name = "C", Salary = 4000 };
+            Customer[] CustArray = new Customer[3];
+            CustArray[0] = a;
+            CustArray[1] = b;
+            CustArray[2] = c;
+
+            dictCust.Add(a.ID, a);
+            dictCust.Add(b.ID, b);
+            dictCust.Add(c.ID, c);
+
+            foreach (KeyValuePair<int, Customer> item in dictCust)
+            {
+                Console.WriteLine("ID : "+ item.Key + " , Name : " + item.Value.Name);               
+            }
+
+            foreach (Customer cust in dictCust.Values)
+            {
+                Console.WriteLine("ID : "+ cust.ID + " , Name : " + cust.Name);               
+            }
+
+            Console.WriteLine("Member with ID 1 :" + dictCust[1].Name);
+            Console.WriteLine("Member with Name B :" + dictCust.FirstOrDefault(x=>x.Name == "B")?.ID );
+            Console.WriteLine("Member with salary > 3K :" + dictCust.Count(x => x.Salary > 3000) );
+            
+            Customer x;
+            if (dictCust.TryGetValue(3, out x ))
+            {
+                Console.WriteLine("Member with ID 3 :" + x.Name);
+            }else
+            {
+                Console.WriteLine("No Key Member found");
+            }
+
+            dictCust = CustArray.ToDictionary( c => c.ID, c=>c );
+            CustArray = dictCust.ToArray(c=>c);
+            List<Customer> lstCust = dictCust.ToList(c=>c);
+            Console.WriteLine("Member with Name B :"  );
+
+            dictCust.Remove(3);
+            dictCust.Clear();
         }
+    }
+
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
     }
 }
