461897984f85749bbe67ab93195e64a346574a40
Shivang Kheradiya <31017420+shivangKheradiya@users.noreply.github.com>
2025-09-16 11:15:53 +0000

    CSharpBasics_T075 to CSharpBasics_T076

--- CSharp/CSharpBasics/CSharpBasics_T075/Program.cs
+++ CSharp/CSharpBasics/CSharpBasics_T075/Program.cs
@@ -10,6 +10,34 @@ namespace CSharpBasics_T075
     {
         static void Main(string[] args)
         {
+            Customer a = new Customer(){ ID = 1, Name = "A", Salary = 5000 };
+            Customer b = new Customer(){ ID = 2, Name = "B", Salary = 3000 };
+            Customer c = new Customer(){ ID = 3, Name = "C", Salary = 4000 };
+            Customer d = new Customer(){ ID = 4, Name = "D", Salary = 4000 };
+            
+            List<Customer> lstCust = new List<Customer>(2);
+            lstCust.Add(a);
+            lstCust.Add(b);
+            lstCust.Add(c);
+            lstCust.Add(d);
+
+            bool containB = lstCust.Contains(b);
+            bool exist = lstCust.Exists(x=>x.Salary> 3000);
+            Customer custB = lstCust.Find(x=>x.Name == "B");
+            Customer custD = lstCust.FindLast(x=>x.Salary == 4000);
+            List<Customer> cust4000 = lstCust.FindAll(x=>x.Salary == 4000);
+            int idx = lstCust.FindIndex(x=>x.Salary == 4000);
+            int idy = lstCust.FindLastIndex(x=>x.Salary == 4000);
+
+            Customer[] custArray = lstCust.ToArray();
+            Dictionary<int, Customer> custArray = lstCust.ToDictionary(cst => cst.ID, cst => cst);
+        }
+
+        public class Customer
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public int Salary { get; set; }
         }
     }
 }
--- CSharp/CSharpBasics/CSharpBasics_T076/Program.cs
+++ CSharp/CSharpBasics/CSharpBasics_T076/Program.cs
@@ -10,6 +10,39 @@ namespace CSharpBasics_T076
     {
         static void Main(string[] args)
         {
+            Customer a = new Customer(){ ID = 1, Name = "A", Salary = 5000 };
+            Customer b = new Customer(){ ID = 2, Name = "B", Salary = 3000 };
+            ExCustomer c = new ExCustomer(){ ID = 3, Name = "C", Salary = 4000 };
+            ExCustomer d = new ExCustomer(){ ID = 4, Name = "D", Salary = 4000 };
+
+            List<Customer> lstCust = new List<Customer>();
+            lstCust.Add(a);
+            lstCust.Add(b);
+
+            List<ExCustomer> lstExCust = new List<ExCustomer>();
+            lstExCust.Add(c);
+            lstExCust.Add(d);
+
+            List<Customer> allCust = new List<Customer>();
+            allCust.AddRange(lstCust);
+            allCust.AddRange(lstExCust);
+            
+            List<Customer> rangeCust = allCust.GetRange(0,3);
+            allCust.AddRange(0,rangeCust);
+            allCust.RemoveAt(0);
+            allCust.RemoveRange(0,2);
+            allCust.Clear();
         }
+    }
+
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+    }
+
+    public class ExCustomer: Customer
+    {
     }
 }
