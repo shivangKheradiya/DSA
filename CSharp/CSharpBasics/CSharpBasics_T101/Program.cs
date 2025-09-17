146473ab42d1bd603ebe14387591e1abb9523b8d
Shivang Kheradiya <31017420+shivangKheradiya@users.noreply.github.com>
2025-09-17 12:38:26 +0000

    CSharpBasics_T101 to CSharpBasics_T102

--- CSharp/CSharpBasics/CSharpBasics_T101/Program.cs
+++ CSharp/CSharpBasics/CSharpBasics_T101/Program.cs
@@ -10,7 +10,28 @@ namespace CSharpBasics_T101
     {
         static void Main(string[] args)
         {
-            
+            int chrs = CountChar();
+            Console.WriteLine("Char count :" + chrs );
+            asyncTaskRun();
+            Thread.Sleep(10000);
+        }
+
+        public async void asyncTaskRun(){
+            Task<int> myTask = new Task<int>(CountChar);
+            myTask.Start();
+            int chrs = await myTask;
+            Console.WriteLine("Char count :" + chrs );
+        }
+
+        public int CountChar(){
+            int count = 0;
+            using (StreamReader reader = new StreamReader(@"C:\db\x.txt"))
+            {
+                string content = reader.ReadToEnd();
+                count += content.Length;
+                Thread.Sleep(5000);
+            }
+            return count;
         }
     }
 }
--- CSharp/CSharpBasics/CSharpBasics_T102/Program.cs
+++ CSharp/CSharpBasics/CSharpBasics_T102/Program.cs
@@ -8,8 +8,37 @@ namespace CSharpBasics_T102
 {
     internal class Program
     {
+        public int counts { get; set; } = 0;
+
         static void Main(string[] args)
         {
+            Thread T1 = new Thread( ()=> {
+                counts = CountChar();
+                // Method 1
+                // Action action = () => Console.WriteLine(counts + " no of chars.");
+                Action action = new Action(ConsoleWrite);
+                this.BeginInvoke(action);
+            });
+            T1.Start();
+            int chrs = counts;
+            Console.WriteLine("Char count :" + chrs );
+
+            Thread.Sleep(10000);
+        }
+
+        private void ConsoleWrite(){
+            Console.WriteLine(counts + " no of chars.");
+        }
+
+        public int CountChar(){
+            int count = 0;
+            using (StreamReader reader = new StreamReader(@"C:\db\x.txt"))
+            {
+                string content = reader.ReadToEnd();
+                count += content.Length;
+                Thread.Sleep(5000);
+            }
+            return count;
         }
     }
 }
